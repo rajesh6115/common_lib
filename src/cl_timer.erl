6e096285b907d3929d364d6702babc517530b8cf
Rajesh Kumar Sahoo <rajesh@3mdigital.co.in>
2017-03-21 17:10:51 +0530

    [deprecated] replacing now as it is depricated

--- src/cl_timer.erl
+++ src/cl_timer.erl
@@ -38,9 +38,9 @@
 %%% UTILITY EXPORTS
 %%%-----------------------------------------------------------------------------
 tc(Fun) ->
-    Before = erlang:now(),
+    Before = erlang:timestamp(),
     Val = (catch Fun()),
-    After = erlang:now(),
+    After = erlang:timestamp(),
     {timer:now_diff(After, Before), Val}.
 
 
@@ -55,7 +55,7 @@ tc_avg(M, F, A, N) when N > 0 ->
 
 
 then(TimeLapse) ->
-    {MegaSecs, Secs, MicroSecs} = now(),
+    {MegaSecs, Secs, MicroSecs} = erlang:timestamp(),
     TotalMicroSecs = MicroSecs + TimeLapse,
     SecsIncr = TotalMicroSecs div 1000000,
     MicroSecsRest = TotalMicroSecs - (SecsIncr * 1000000),
@@ -66,7 +66,7 @@ then(TimeLapse) ->
 
 
 tstamp() ->
-    {A, B, C} = now(),
+    {A, B, C} = erlang:timestamp(),
     (((A * 1000000) * 1000) + (B * 1000) + (C div 1000)).
 
 %%%-----------------------------------------------------------------------------
